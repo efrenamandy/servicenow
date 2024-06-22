9824101095ede8103c40d5f4124bf812f9df08b2
Efren Amandy <efrenamandy@gmail.com>
2024-06-22 12:02:15 +0800

    Update get_all_child_tables.js

--- get_all_child_tables.js
+++ get_all_child_tables.js
@@ -1,3 +1,7 @@
+//Only works in Global
 var taskTables = new TableUtils('task').getAllExtensions();
 gs.include("j2js");
 taskTables = j2js(taskTables);
+
+//For Scoped
+new GlideTableHierarchy('task').getAllExtensions();
